0a0efc848c06a7223a8f49281c7b7ed8d0d2a205
Frost <murtada.najm99@gmail.com>
2026-01-24 15:09:14 +0300

    fixed gh-pages issue

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/waqitly-dashboard">
     <QueryClientProvider client={queryClient}>
       <ThemeProvider 
         attribute="class" 
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/waqitly-dashboard/",
+  base: "/waqitly-dashboard",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
